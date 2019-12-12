f456ab35fad0b6dddd551014e6df0bd9ce8cf954
Chandu J S <app.coder@outlook.in>
2019-12-12 14:57:25 +0400

    basic layout done

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,54 +1,44 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
+// import 'package:content_placeholder/placeholder_item.dart';
 
-import 'package:flutter/services.dart';
-import 'package:content_placeholder/content_placeholder.dart';
+import 'placeholder_item.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(App());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await ContentPlaceholder.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+      debugShowCheckedModeBanner: false,
+      home: View(),
+    );
+  }
+}
+
+class View extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            ContentPlaceholder(isAnimationEnabled: false,),
+            ContentPlaceholder(),
+            ContentPlaceholder(height: 10),
+            // ContentPlaceholder(width: 50),
+            ContentPlaceholder(
+              context: context,
+              child: Column(
+                children: <Widget>[
+                  ContentPlaceholder.block(),
+                  ContentPlaceholder.block(height: 100),
+                  Text('Hi')
+                  // ContentPlaceholder.block(width: 200),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/content_placeholder.dart
+++ lib/content_placeholder.dart
@@ -1,13 +1,10 @@
-import 'dart:async';
+import 'package:flutter/material.dart';
 
-import 'package:flutter/services.dart';
-
-class ContentPlaceholder {
-  static const MethodChannel _channel =
-      const MethodChannel('content_placeholder');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+class PlaceholderItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
   }
 }
